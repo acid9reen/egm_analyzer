d010cc51754d0231488e7a0120af80cf338e74ba
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2023-02-19 19:46:49 +0300

    Combine all objects for processing

--- egm_analyzer/signal_processor.py
+++ egm_analyzer/signal_processor.py
@@ -1,8 +1,12 @@
+import csv
+from pathlib import Path
 from typing import Generator
 
 import numpy as np
+from tqdm import tqdm
 
 from egm_analyzer.models.model import PredictionModel
+from egm_analyzer.pred_processor import compress
 
 
 seconds = int
@@ -20,10 +24,13 @@ def batcher(
         start_index = batch_index * step
         stop_index = start_index + step
 
-        batch.append(signal[start_index:stop_index])
+        batch.append([signal[start_index:stop_index]])
 
         if len(batch) % batch_size == 0:
-            yield np.array(batch)
+            try:
+                yield np.array(batch)
+            finally:
+                batch = []
 
     yield np.array(batch)
 
@@ -33,15 +40,32 @@ class SignalProcessor(object):
             self,
             model: PredictionModel,
             batch_size: int,
+            output_file: Path,
             signal_frequency: int = 5_000,
             signal_length: seconds = 2,
+            threshold: float = 0.5,
     ) -> None:
         self._model = model
         self._batch_size = batch_size
         self._step = signal_frequency * signal_length
+        self._threshold = threshold
+        self._output_file = output_file
+
+    def _write_result(self, result: list[int]) -> None:
+        with open(self._output_file, 'a', newline='') as out:
+            csv_writer = csv.writer(out, dialect='excel')
+            csv_writer.writerow(result)
 
     def process(self, signal: np.ndarray) -> None:
-        for channel_index, channel in enumerate(signal):
+        for channel_index, channel in tqdm(
+                enumerate(signal), total=len(signal), colour='green',
+        ):
+            channel_predictions = []
+
             for batch in batcher(channel, self._batch_size, self._step):
                 prediction_batch = self._model.predict(batch)
-                _ = prediction_batch.flatten()
+                channel_predictions.append(prediction_batch.flatten())
+
+            channel_prediction = np.hstack(channel_predictions)
+            result = compress(channel_prediction, self._threshold, 0)
+            self._write_result(result)
