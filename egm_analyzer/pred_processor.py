9fc449780740c6892fc342dae9086f5bf4e34b07
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2023-03-08 21:09:55 +0300

    Modify derivative calculation algo

--- egm_analyzer/pred_processor.py
+++ egm_analyzer/pred_processor.py
@@ -1,43 +1,64 @@
-from typing import Callable
-
 import numpy as np
 
 
-def mean_compressor(pred: np.ndarray) -> int:
-    return len(pred) // 2
+def first_derivative(func: np.ndarray) -> float:
 
+    maxi_left = np.max(func[0], func[1])
+    mini_right = np.min(func[3], func[4])
 
-def max_compressor(pred: np.ndarray) -> int:
-    return int(np.argmax(pred))
+    return mini_right - maxi_left
 
 
 def compress(
         pred: np.ndarray,
-        treshold: float,
-        skew: int,
-        compressor: Callable[[np.ndarray], int] = max_compressor,
+        signal: np.ndarray,
+        window_size: int,
 ) -> list[int]:
 
+    pred = np.sort(pred)
+
     peaks_indexes = []
-    flag = False
+    group_start = pred[0]
+    founded_end = False
 
-    for index, elem in enumerate(pred):
+    for index, elem in enumerate(pred[2:-2]):
+        founded_end = False
 
-        if (elem > treshold) and (not flag):
-            index_start = index
-            flag = True
+        if (pred[index + 1] - elem > window_size):
+            group_end = elem
 
-        if (elem < treshold) and (flag):
-            index_end = index
-            flag = False
+            if group_end - group_start > 5:
 
-            realtive_peak_index = compressor(pred[index_start:index_end])
-            peaks_indexes.append(index_start + skew + realtive_peak_index)
+                min_derivative = 1_000_000_000
+                min_derivative_idx = 0
 
-    if flag:
-        realtive_peak_index = compressor(pred[index_start: len(pred)])
-        peaks_indexes.append(index_start + realtive_peak_index)
+                for idx in range(group_start - 2, group_end + 2):
+                    derivative = first_derivative(signal[idx:idx + 5])
+                    if derivative < min_derivative:
+                        min_derivative = derivative
+                        min_derivative_idx = idx + 2
+
+                group_start = pred[index + 1]
+                founded_end = True
+                peaks_indexes.append(min_derivative_idx)
+            else:
+                group_start = pred[index + 1]
+
+    if not founded_end:
+
+        group_end = min(pred[-1], len(signal) - 2)
+
+        if group_end - group_start > 5:
+
+            min_derivative = 1_000_000_000
+            min_derivative_idx = 0
+
+            for idx in range(group_start - 2, group_end + 2):
+                derivative = first_derivative(signal[idx:idx + 5])
+                if derivative < min_derivative:
+                    min_derivative = derivative
+                    min_derivative_idx = idx + 2
+
+                peaks_indexes.append(min_derivative_idx)
 
     return peaks_indexes
-
-# TODO fix close preds in different samples
