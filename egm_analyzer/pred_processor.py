0ede515d9df4054bb309ec3ae7a41084879cecc7
Anton Rybkin <tosharybking@gmail.com>
2023-03-26 16:19:11 +0300

    Add spline derivative

--- egm_analyzer/pred_processor.py
+++ egm_analyzer/pred_processor.py
@@ -1,75 +1,42 @@
 from typing import Protocol
 
 import numpy as np
-
-
-class Derivative(Protocol):
-    @property
-    def kernel_size(self) -> int:
-        ...
-
-    def calculate_approximation(self, signal_cutout: np.ndarray) -> float:
-        ...
-
-
-class ThreePointDerivative:
-    @property
-    def kernel_size(self) -> int:
-        return 3
-
-    def calculate_approximation(self, signal_cutout: np.ndarray) -> float:
-        return signal_cutout[2] - signal_cutout[0]
-
-
-class FivePointDerivative:
-    @property
-    def kernel_size(self) -> int:
-        return 5
-
-    def calculate_approximation(self, signal_cutout: np.ndarray) -> float:
-        maxi_left = max(signal_cutout[0], signal_cutout[1])
-        mini_right = min(signal_cutout[3], signal_cutout[4])
-
-        return mini_right - maxi_left
+from scipy.interpolate import CubicSpline
 
 
 class Compressor:
     def __init__(
             self,
-            derivative_calculator: Derivative = ThreePointDerivative(),
             window_size: int = 100,
-            frequency: int = 5000,
+            signal_frequency: int = 5000,
+            target_frequency: int = 20_000,
     ) -> None:
-        self._derivative = derivative_calculator
+
         self._window_size = window_size
-        self._padding_size = self._derivative.kernel_size // 2
-        self._indices_to_microseconds_constant = int(1 / frequency * 1e6)
+        self._indices_to_microseconds_constant = int(1 / target_frequency * 1e6)
+        self._signal_to_target_frequency = target_frequency / signal_frequency
 
     def find_relative_minimum_derivative_index(
             self,
             signal_cutout: np.ndarray,
     ) -> int | None:
-        if len(signal_cutout) < self._derivative.kernel_size:
+        if len(signal_cutout) < 5:
             return None
+        
 
-        minimum_derivative = self._derivative.calculate_approximation(
-            signal_cutout[:self._derivative.kernel_size],
+        indexes = range(len(signal_cutout))
+        cs = CubicSpline(indexes, signal_cutout)
+        
+        high_res_indexes = np.linspace(
+            0,
+            len(signal_cutout),
+            int(self._signal_to_target_frequency * len(signal_cutout)),
+            endpoint=False,
         )
-        minimum_derivative_index = self._padding_size
-        start_index = minimum_derivative_index + 1
-
-        for point_index in range(start_index, len(signal_cutout) - self._padding_size):
-            derivative = self._derivative.calculate_approximation(
-                signal_cutout[
-                    point_index - self._padding_size:point_index + self._padding_size + 1
-                ],
-            )
-
-            if derivative <= minimum_derivative:
-                minimum_derivative = derivative
-                minimum_derivative_index = point_index
-
-        return minimum_derivative_index
+        
+        abscis_min_der_index = np.argmin(cs(high_res_indexes, 1))
+        
+        return abscis_min_der_index
 
     def compress(self, preds: np.ndarray, signal: np.ndarray) -> list[int]:
         if len(preds) < 1:
@@ -88,14 +55,15 @@ class Compressor:
 
             relative_minimum_derivative_index = (
                 self.find_relative_minimum_derivative_index(
-                    signal[max(group_start - self._padding_size, 0):group_end + self._padding_size],
+                    signal[group_start : group_end],
                 )
             )
 
             if relative_minimum_derivative_index is not None:
                 peaks_indexes.append(
                     (
-                        relative_minimum_derivative_index + group_start - self._padding_size
+                        relative_minimum_derivative_index
+                        + self._signal_to_target_frequency * group_start
                     ) * self._indices_to_microseconds_constant,
                 )
 
@@ -104,14 +72,15 @@ class Compressor:
 
         relative_minimum_derivative_index = (
             self.find_relative_minimum_derivative_index(
-                signal[max(group_start - self._padding_size, 0):group_end + self._padding_size],
+                signal[group_start : group_end],
             )
         )
 
         if relative_minimum_derivative_index is not None:
             peaks_indexes.append(
                 (
-                    relative_minimum_derivative_index + group_start - self._padding_size
+                    relative_minimum_derivative_index
+                    + self._signal_to_target_frequency * group_start
                 ) * self._indices_to_microseconds_constant,
             )
 
